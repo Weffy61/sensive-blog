12b279ff9bb351041748caa07e0186d461790a4e
Weffy61 <weffy@mail.ru>
2023-10-23 20:42:30 +0300

    Use realated name with comments

--- blog/views.py
+++ blog/views.py
@@ -51,7 +51,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')), slug=slug)
-    comments = Comment.objects.filter(post=post).prefetch_related('author').order_by('-author')
+    comments = post.comments.prefetch_related('author').order_by('-author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
